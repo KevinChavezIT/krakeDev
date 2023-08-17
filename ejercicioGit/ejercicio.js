22ff9d5074335bf367ba2f1a72bc1fac47a242e6
KevinChavezIT <hc7657819@gmail.com>
2023-08-17 16:23:36 -0500

    agg boton saludar

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,6 +1,8 @@
 saludar = function() {
+    let cmpcSaludo;
+
+    cmpcSaludo = document.getElementById("imgSaludo");
+    cmpcSaludo.src = "./imagenes/giphy.gif";
 
     console.log("ingresar saludar");
-
-
 }
