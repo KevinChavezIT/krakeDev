683ade9f788f716d52541c2f2a93dd054005a8e5
KevinChavezIT <hc7657819@gmail.com>
2023-08-18 00:40:51 -0500

    correcion de mostrar textos y limpiar usando utilitarios.js

--- ejercicioVenta/puntoDeVenta.js
+++ ejercicioVenta/puntoDeVenta.js
@@ -18,36 +18,38 @@ calcularValorTotal = function() {
 
     porcentajeDescuento = recuperarInt("txtPorcentajeDescuento");
 
+
     valorSubtotal = calcularSubtotal(precioProducto, cantidad);
-    document.getElementById("lblSubtotal").innerText = "$ " + valorSubtotal;
+    mostrarTexto("lblSubtotal", valorSubtotal);
 
 
     valorDescuento = calcularValorDescuento(valorSubtotal, porcentajeDescuento);
-    document.getElementById("lblDescuento").innerText = "$ " + valorDescuento;
+    mostrarTexto("lblDescuento", valorDescuento);
 
 
     valorIVA = calcularIva(valorSubtotal, valorDescuento);
-    document.getElementById("lblValorIVA").innerText = "$ " + valorIVA;
+    mostrarTexto("lblValorIVA", valorIVA);
 
 
     valorTotal = calcularTotal(valorSubtotal, valorDescuento, valorIVA);
-    document.getElementById("lblTotal").innerText = "$ " + valorTotal;
+    mostrarTexto("lblTotal", valorTotal);
 
 
-    document.getElementById("lblResumen").innerHTML = "Resumen de compra: " + "Valor a pagar por " + cantidad + " " + nombreProducto +
+    let resumen = "Resumen de compra: " + "Valor a pagar por " + cantidad + " " + nombreProducto +
         " con el " + porcentajeDescuento + "% de Descuento es de: USD " + valorTotal;
+    mostrarTexto("lblResumen", resumen);
 
 }
 limpiar = function() {
 
-    document.getElementById("txtProducto").value = " ";
-    document.getElementById("txtPrecio").value = "0.0";
-    document.getElementById("txtCantidad").value = "0";
-    document.getElementById("txtPorcentajeDescuento").value = "0";
-    document.getElementById("lblSubtotal").innerText = "0.0";
-    document.getElementById("lblDescuento").innerText = "0.0";
-    document.getElementById("lblValorIVA").innerText = "0.0";
-    document.getElementById("lblTotal").innerText = "0.0";
-    document.getElementById("lblResumen").innerText = "Resumen de compra: ";
+    mostrarTextoEnCaja("txtProducto", " ");
+    mostrarTextoEnCaja("txtPrecio", "0.0");
+    mostrarTextoEnCaja("txtCantidad", "0");
+    mostrarTextoEnCaja("txtPorcentajeDescuento", "0");
+    mostrarTexto("lblSubtotal", "0.0");
+    mostrarTexto("lblDescuento", "0.0");
+    mostrarTexto("lblValorIVA", "0.0");
+    mostrarTexto("lblTotal", "0.0");
+    mostrarTexto("lblResumen", "Resumen de compra: ");
 
 }
