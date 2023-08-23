04c5a3d5f871bb28fab505e5ef71866169de22f5
KevinChavezIT <hc7657819@gmail.com>
2023-08-22 22:31:30 -0500

    promedio notas Version 2

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -14,10 +14,23 @@ calcularPromedioNotas = function() {
     resultFormateado = promedio.toFixed(2);
     cambiarTexto("lblPromedio", resultFormateado + " Promedio Final");
 
-    if (resultFormateado > 7) {
+    if (resultFormateado < 5 && resultFormateado > 0) {
 
-        cambiarImagen("imgPromedio", "./imagenesPromedio/aprobado.gif");
-    } else {
         cambiarImagen("imgPromedio", "./imagenesPromedio/reprobado.gif");
+        cambiarTexto("lblResultado", "REPROBADO");
+
+    } else if (resultFormateado >= 5 && resultFormateado <= 8) {
+
+        cambiarImagen("imgPromedio", "./imagenesPromedio/maso.gif");
+        cambiarTexto("lblResultado", "BUEN TRABAJO");
+
+    } else if (resultFormateado > 8 && resultFormateado <= 10) {
+
+        cambiarImagen("imgPromedio", "./imagenesPromedio/excelente.gif");
+        cambiarTexto("lblResultado", "BUEN EXCELENTE");
+    } else {
+        cambiarImagen("imgPromedio", "./imagenesPromedio/incorrecto.gif");
+        cambiarTexto("lblResultado", "DATOS INCORRECTOS");
     }
+
 }
