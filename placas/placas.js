f7c60f011a14a33f4c0a1ccbc63361b3d4db1371
KevinChavezIT <hc7657819@gmail.com>
2023-08-29 19:30:33 -0500

    mostrar validacion de placa en pantalla

--- placas/placas.js
+++ placas/placas.js
@@ -8,10 +8,11 @@ validarPlaca = function() {
     erroresEstructura = validarEstructura(placa);
 
     if (erroresEstructura === null) {
-        alert("ESTRUCTURA VALIDA");
+        lblValidacion.innerText = ("ESTRUCTURA VALIDA");
         document.getElementById("lblErrores").innerText = "";
+
     } else {
         document.getElementById("lblErrores").innerHTML = erroresEstructura;
-        alert("ESTRUCTURA INCORRECTA");
+        lblValidacion.innerText = ("ESTRUCTURA INCORRECTA");
     }
 }
